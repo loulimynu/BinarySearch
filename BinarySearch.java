5c984beb3e03a9a84897444af3c7b180be5d69fd
loulimynu <64753817+loulimynu@users.noreply.github.com>
2022-04-26 11:57:06 -0300

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,43 @@
+package project.binarysearch;
+
+/**
+ *
+ * @author white
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int x, left, right;
+        int[]arr = new int[20];
+    }
+    public static boolean binarySearchRecursive(int[] arr, int x, int left, int right) {
+        if (left > right) {
+            return false;
+        }
+        int mid = left + ((right - left) / 2);
+        if (arr[mid] == x) {
+            return true;
+        } else if (x < arr[mid]) {
+            return binarySearchRecursive(arr, x, left, mid - 1);
+        } else {
+            return binarySearchRecursive(arr, x, right, mid + 1);
+        }
+    }
+    public static boolean binarySearchInterative(int[] arr, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+            if (arr[mid] == x) {
+                return true;
+            } else if (x < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
